1e61ef66c01b1c42d3703360ce8bbb487c536f9f
Laurent <laurent.rothenbug@gmail.com>
2021-07-21 14:49:08 +0200

    menu et not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,32 @@ import {SkillsSettingComponent} from './skills-setting/skills-setting.component'
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import { SkillsComponent } from './skills/skills.component';
+import { SkillsSearchComponent } from './skills-search/skills-search.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
+const appRoutes = [
+  { path: 'skillsSetting', component: SkillsSettingComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'skillsSearch', component: SkillsSearchComponent },
+  { path: '', component: SkillsSettingComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo:'/not-found'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsSettingComponent
+    SkillsSettingComponent,
+    SkillsComponent,
+    SkillsSearchComponent,
+    FourOhFourComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      {path: '', component: SkillsSettingComponent}
-    ])
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
